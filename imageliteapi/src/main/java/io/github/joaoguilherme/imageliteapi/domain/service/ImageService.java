7a7fde5a21fe1744ed48920566fd5f1398b7e897
joaoGuilhermeOliveira <joaogoliveira14@gmail.com>
2024-11-06 17:40:38 -0300

    feature: adicionado método de pesquisa em Service

--- imageliteapi/src/main/java/io/github/joaoguilherme/imageliteapi/domain/service/ImageService.java
+++ imageliteapi/src/main/java/io/github/joaoguilherme/imageliteapi/domain/service/ImageService.java
@@ -1,7 +1,9 @@
 package io.github.joaoguilherme.imageliteapi.domain.service;
 
 import io.github.joaoguilherme.imageliteapi.domain.entity.Image;
+import io.github.joaoguilherme.imageliteapi.domain.enums.ImageExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ImageService {
@@ -9,5 +11,7 @@ public interface ImageService {
     Image save(Image image);
 
     Optional<Image> getBydId(String id);
+
+    List<Image> search(ImageExtension extension, String query);
 
 }
--- imageliteapi/src/main/java/io/github/joaoguilherme/imageliteapi/domain/service/ImageServiceImpl.java
+++ imageliteapi/src/main/java/io/github/joaoguilherme/imageliteapi/domain/service/ImageServiceImpl.java
@@ -1,12 +1,14 @@
 package io.github.joaoguilherme.imageliteapi.domain.service;
 
 import io.github.joaoguilherme.imageliteapi.domain.entity.Image;
+import io.github.joaoguilherme.imageliteapi.domain.enums.ImageExtension;
 import io.github.joaoguilherme.imageliteapi.domain.service.ImageService;
 import io.github.joaoguilherme.imageliteapi.infra.repository.ImageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -23,5 +25,10 @@ public class ImageServiceImpl implements ImageService {
     @Override
     public Optional<Image> getBydId(String id) {
         return imageRepository.findById(id);
+    }
+
+    @Override
+    public List<Image> search(ImageExtension extension, String query) {
+        return imageRepository.findByExtensionAndNameOrTagsLike(extension, query);
     }
 }
