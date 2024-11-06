ad775695719fc3fe3191712912212a0307cb5bb5
joaoGuilhermeOliveira <joaogoliveira14@gmail.com>
2024-11-06 14:16:46 -0300

    feature: salvando imagem através da api

--- imageliteapi/src/main/java/io/github/joaoguilherme/imageliteapi/controller/ImagesController.java
+++ imageliteapi/src/main/java/io/github/joaoguilherme/imageliteapi/controller/ImagesController.java
@@ -1,6 +1,11 @@
 package io.github.joaoguilherme.imageliteapi.controller;
 
+import io.github.joaoguilherme.imageliteapi.domain.entity.Image;
+import io.github.joaoguilherme.imageliteapi.domain.enums.ImageExtension;
+import io.github.joaoguilherme.imageliteapi.domain.service.ImageService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,18 +13,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.print.attribute.standard.Media;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/images")
 @Slf4j
+@RequiredArgsConstructor
 public class ImagesController {
 
+    private final ImageService imageService;
     @PostMapping
-    public ResponseEntity save(@RequestParam("file") MultipartFile file, @RequestParam("name") String name, @RequestParam("tags")List<String> tags) {
+    public ResponseEntity save(@RequestParam("file") MultipartFile file, @RequestParam("name") String name, @RequestParam("tags")List<String> tags) throws IOException {
         log.info("Imagem recebida: name: {}, size: {}", file.getOriginalFilename(), file.getSize());
-        log.info("Nome definido para a imagem: {}", name);
-        log.info("Tags: {}", tags);
+
+
+        Image image = Image.builder().name(name).tags(String.join(",", tags)).extension(ImageExtension.valuOf(MediaType.valueOf(file.getContentType()))).file(file.getBytes()).size(file.getSize()).build();
+
+        imageService.save(image);
         return ResponseEntity.ok().build();
     }
 }
