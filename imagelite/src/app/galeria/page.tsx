9bf5431a182340987cd96d72becb5df78f0aabc9
joaoGuilhermeOliveira <joaogoliveira14@gmail.com>
2024-11-07 17:32:38 -0300

    feature: componente ImageCard criado

--- imagelite/src/app/galeria/page.tsx
+++ imagelite/src/app/galeria/page.tsx
@@ -1,9 +1,19 @@
-import { Template } from '@/components'
+import { Template, ImageCard } from '@/components'
 
 export default function GaleriaPage() {
     return(
         <Template>
             <h1>Galeria</h1>
+            <section className='grid grid-cols-4 gap-8'>
+                <ImageCard nome='Caminho' tamanho='1MB' dataUpload='07/11/2024' src='https://www.aaronreedphotography.com/images/xl/Sweet-Dreams-2022.jpg'/>
+                <ImageCard nome='Caminho' tamanho='1MB' dataUpload='07/11/2024' src='https://www.aaronreedphotography.com/images/xl/Sweet-Dreams-2022.jpg'/>
+                <ImageCard nome='Caminho' tamanho='1MB' dataUpload='07/11/2024' src='https://www.aaronreedphotography.com/images/xl/Sweet-Dreams-2022.jpg'/>
+                <ImageCard nome='Caminho' tamanho='1MB' dataUpload='07/11/2024' src='https://www.aaronreedphotography.com/images/xl/Sweet-Dreams-2022.jpg'/>
+                <ImageCard nome='Caminho' tamanho='1MB' dataUpload='07/11/2024' src='https://www.aaronreedphotography.com/images/xl/Sweet-Dreams-2022.jpg'/>
+                <ImageCard nome='Caminho' tamanho='1MB' dataUpload='07/11/2024' src='https://www.aaronreedphotography.com/images/xl/Sweet-Dreams-2022.jpg'/>
+                <ImageCard nome='Caminho' tamanho='1MB' dataUpload='07/11/2024' src='https://www.aaronreedphotography.com/images/xl/Sweet-Dreams-2022.jpg'/>
+                <ImageCard nome='Caminho' tamanho='1MB' dataUpload='07/11/2024' src='https://www.aaronreedphotography.com/images/xl/Sweet-Dreams-2022.jpg'/>
+            </section>
         </Template>
 )
 }
